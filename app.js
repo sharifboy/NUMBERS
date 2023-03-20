cde3cf94fa4a3aae6ed340c0bf33a8d4ab5fa52b
sharifboy <sharifboy@gmail.com>
2023-03-20 07:40:06 +0500

    made initial layouts

---
+++ app.js
@@ -0,0 +1,35 @@
+let radius = 10;
+const pi = 3.14;
+
+// console.log(radius, pi);
+
+// math operators +,-,/,*,**,%
+
+// console.log(10 / 2);
+
+// let result = 10 % 7
+// let result = pi * radius ** 2
+
+// order of operation
+
+// let result = 5 * (10 - 5)  ** 3
+
+let likes = 10;
+
+// likes = likes + 1
+// likes++;
+// likes--;
+// likes = likes + 10
+// likes += 12;
+// likes -= 8;
+// likes /= 5;
+// likes *= 3;
+
+// console.log(likes);
+
+// NaN - not a number 
+
+console.log(5 / 'salom');
+
+let result = 'the blog has ' + likes + ' likes' 
+console.log(result);
